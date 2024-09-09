f5506e80d30a41f321cdf39a8bd686b826e1fffb
Vanessa <160286052+AmaCodez@users.noreply.github.com>
2024-09-09 10:21:04 -0400

    'Add task array and list class'

--- src/mytask.js
+++ src/mytask.js
@@ -1,48 +1,14 @@
 const allTask = () => {
-    const inputTask = document.querySelector('.input-task');
-const addBtn = document.querySelector('.addBtn');
-const taskContainer = document.querySelector('.task-container');
+    const myTask = [];
 
-    function taskValue () {
-    const task = inputTask.value;
-    inputTask.value = '';
-
-    return {task};
-};
-
-addBtn.addEventListener('click', () => {
-   const {task} = taskValue();
-
-    const taskListCont = document.createElement('div');
-    taskListCont.classList.add('list-container');
-
-    const checklistCont = document.createElement('div');
-    checklistCont.classList.add('checklist-container');
-
-    const checklistInput = document.createElement('input');
-    checklistInput.type = 'checkbox';
-    checklistInput.id = 'userTask';
-    const checklistLabel = document.createElement('label');
-    checklistLabel.for = 'userTask';
-    checklistLabel.textContent = task;
-
-    const deleteBtn = document.createElement('button');
-    deleteBtn.classList.add('deleteBtn');
-    deleteBtn.textContent = 'delete';
-
-    deleteBtn.addEventListener('click', () => {
-        taskContainer.removeChild(taskListCont);
-    });
-
-    checklistCont.appendChild(checklistInput);
-    checklistCont.appendChild(checklistLabel);
-    taskListCont.appendChild(checklistCont);
-    taskListCont.appendChild(deleteBtn);
-
-   taskContainer.appendChild(taskListCont);
-    
-});
-
+    class Task {
+        constructor (name, date, priority, note) {
+            this.name = name;
+            this.date = date;
+            this.priority = priority;
+            this.note = note;
+        }
+    };
 };
 
 export default allTask;
