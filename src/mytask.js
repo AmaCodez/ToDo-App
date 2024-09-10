30342d744883db1e54b87094569f05d856950741
Vanessa <160286052+AmaCodez@users.noreply.github.com>
2024-09-10 09:39:19 -0400

    'Create addTask function' 'Create displayTask function'

--- src/mytask.js
+++ src/mytask.js
@@ -1,4 +1,7 @@
 const allTask = () => {
+    const taskContainer = document.querySelector('#task-container');
+
+
     const myTask = [];
 
     class TaskList {
@@ -9,6 +12,18 @@ const allTask = () => {
             this.note = note;
         }
     };
+
+    function addTask (name, date, priority, note){ //create a new object and push in myTask array
+        const newTask = new TaskList (name, date, priority, note); 
+        myTask.push(newTask);
+    };
+
+    function displayTask (){
+        for(let i = 0; i < myTask.length; i++) {
+            taskContainer.innerHTML = null;
+        };
+    };
+
 };
 
 export default allTask;
